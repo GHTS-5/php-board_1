43ac7c6df0b5760fb701a9390bda5f76d9f70209
GHTS_5 <ghts5748@gmail.com>
2021-01-12 10:47:03 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <style>
@@ -33,10 +32,10 @@
 </style>
 <body>
     <?php
-        $connect = mysqli_connect('localhost', 'root', '', 'board') or die ("Connect fail");
+        $connect = mysqli_connect('localhost','root','','board') or die ("Connect fail");
         $query = "selct * from board order by number desc";
         $result = $connect->query($query);
-        $total = mysquli_num_rows($result);
+        $total = mysqli_num_rows($result);
 
         session_start();
 
@@ -65,33 +64,37 @@
 
     <tbody>
     <?php
-        while($rows = mysqli_fetch_assec($result)){
-            if($total%2--0){
-?>              <tr class ="even">
-            <?php    }
-            else{
-?>        <tr>
-        <?php } ?>
-        <td width = "50" align="center"><<?php echo $total?></td?>
-        <td width = "500" align="center">
+        while($rows = mysqli_fetch_assoc($result)){ //DB에 저장된 데이터 수 (열 기준)
+                if($total%2==0){
+?>                      <tr class = "even">
+                <?php   }
+                else{
+?>                      <tr>
+                <?php } ?>
+        <td width = "50" align = "center"><?php echo $total?></td>
+        <td width = "500" align = "center">
         <a href = "view.php?number=<?php echo $rows['number']?>">
-        <?php echo $rows['title']?>
-            </td?>
-            <td width = "100" align="center"><?php echo $rows['id']?></td?>
-        <td width = "200-" align="center"><?php echo $rows['date']?></td?>
-        <td width = "50" align="center"><?php echo $rows['hit']?></td?>
+        <?php echo $rows['title']?></td>
+          <td width = "100" align = "center"><?php echo $rows['id']?></td>
+        <td width = "200" align = "center"><?php echo $rows['date']?></td>
+        <td width = "50" align = "center"><?php echo $rows['hit']?></td>
         </tr>
 <?php
-        $total --;
-    }
-    ?>
-    </tbody>
-    </table>
+        $total--;
+        }
+?>
+</tbody>
+</table>
+
+
      
      <div class=text>
      <font style="cursor:hand" onclick="location.href='./write.php'">글쓰기</font>
      </div>
         
+        }
+
+
 </body>
 </html>
 
